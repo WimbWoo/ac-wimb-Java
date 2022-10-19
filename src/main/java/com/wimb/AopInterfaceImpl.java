b17de937281de9c74b81e5903a3e00bb2715a3eb
wimb <wimbwoo@163.com>
2022-10-19 23:58:57 +0800

    dev commit

--- src/main/java/com/wimb/AopInterfaceImpl.java
+++ src/main/java/com/wimb/AopInterfaceImpl.java
@@ -12,5 +12,6 @@ public class AopInterfaceImpl implements AopInterface {
     @Override
     public void say() {
         System.out.println("hallo");
+        System.out.println("dev v1");
     }
 }
